a9bbb0713c0eff46151bfdaee77bdf870276981b
unknown <vpsuri@irusmail.com>
2020-08-09 13:38:40 -0600

    chg for new page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-        return "Hello World!"
+    myName = "Viraj"
+    return render_template('index.html', title='Home', myName=myName)
+
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html', title='Page 1')
